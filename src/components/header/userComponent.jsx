41b70be6f32643eed49c563a48ff6de547335279
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-02 10:53:31 +0100

    added header user menu button

--- src/components/header/userComponent.jsx
+++ src/components/header/userComponent.jsx
@@ -1,12 +1,32 @@
 import { Avatar, AvatarBadge } from '@chakra-ui/react';
 import React from 'react';
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Button
+} from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 const UserComponent = () => {
+    
+    const username = 'Rubén Valls';
+
     return (
         <div className='userheaderimg'>
-            <Avatar size='sm' name='User Name' src='https://www.svgrepo.com/show/313172/user.svg'>
-                <AvatarBadge boxSize='1.25em' bg='green.500' />
-            </Avatar>
+            <Menu>
+                <MenuButton as={Button} variant='transparent' size='sm' rightIcon={<ChevronDownIcon />}>
+                    <Avatar size='sm' name={username} bg='teal.500' src='' id='avatar'>
+                        <AvatarBadge boxSize='1.25em' bg='green.500' />
+                    </Avatar>
+                </MenuButton>
+                <MenuList>
+                    <MenuItem>Profile</MenuItem>
+                    <MenuItem>D/L Mode</MenuItem>
+                    <MenuItem>Log Out</MenuItem>
+                </MenuList>
+            </Menu>
         </div>
     );
 }
