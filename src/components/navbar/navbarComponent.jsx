e0435229e30d7e0f06f563689a76764b67b94b4b
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-02 09:37:46 +0100

    improving navbar with chakra

--- src/components/navbar/navbarComponent.jsx
+++ src/components/navbar/navbarComponent.jsx
@@ -6,29 +6,29 @@ const NavbarComponent = () => {
     return (
         <div className='navbarcontainer'>
             <a href='#'>
-                <img src='https://cdn-icons-png.flaticon.com/512/747/747589.png' alt='homeicon' width='30'/>
-                <Text fontSize='lg' className='navtext'>Home</Text>
+                <img src='https://cdn-icons-png.flaticon.com/512/747/747589.png' alt='homeicon' width='25'/>
+                <Text fontSize='md' className='navtext'>Home</Text>
             </a>
             <Center height='25px'>
                 <Divider orientation='vertical' />
             </Center>
             <a href='#'>
-                <img src='https://cdn-icons-png.flaticon.com/512/1972/1972090.png' alt='worksicon' width='30'/>
-                <Text fontSize='lg' className='navtext'>Works</Text>
+                <img src='https://cdn-icons-png.flaticon.com/512/1972/1972090.png' alt='worksicon' width='25'/>
+                <Text fontSize='md' className='navtext'>Works</Text>
             </a>
             <Center height='25px'>
                 <Divider orientation='vertical' />
             </Center>
             <a href='#'>
-                <img src='https://cdn-icons-png.flaticon.com/512/749/749064.png' alt='coursesicon' width='30'/>
-                <Text fontSize='lg' className='navtext'>Courses</Text>
+                <img src='https://cdn-icons-png.flaticon.com/512/749/749064.png' alt='coursesicon' width='25'/>
+                <Text fontSize='md' className='navtext'>Courses</Text>
             </a>
             <Center height='25px'>
                 <Divider orientation='vertical' />
             </Center>
             <a href='#'>
-                <img src='https://cdn-icons-png.flaticon.com/512/3771/3771152.png' alt='newsicon' width='30'/>
-                <Text fontSize='lg' className='navtext'>News</Text>
+                <img src='https://cdn-icons-png.flaticon.com/512/3771/3771152.png' alt='newsicon' width='25'/>
+                <Text fontSize='md' className='navtext'>News</Text>
             </a>
         </div>
     );
