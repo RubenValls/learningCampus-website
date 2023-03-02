3f8538c91350e4c260cd1bfda8348ffb9d4b252d
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-02 10:34:04 +0100

    added lateral navbar to more info

--- src/components/navbar/navbarComponent.jsx
+++ src/components/navbar/navbarComponent.jsx
@@ -1,35 +1,70 @@
 import React from 'react';
-import { Divider, Center } from '@chakra-ui/react'
+import { Divider, Center, useDisclosure } from '@chakra-ui/react'
 import { Text } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+} from '@chakra-ui/react'
+import LogoComponent from '../header/logoComponent';
 
 const NavbarComponent = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+
     return (
         <div className='navbarcontainer'>
-            <a href='#'>
+            <Button colorScheme='teal' variant='ghost' className='navbtn'>
                 <img src='https://cdn-icons-png.flaticon.com/512/747/747589.png' alt='homeicon' width='25'/>
                 <Text fontSize='md' className='navtext'>Home</Text>
-            </a>
+            </Button>
             <Center height='25px'>
                 <Divider orientation='vertical' />
             </Center>
-            <a href='#'>
+            <Button colorScheme='teal' variant='ghost' className='navbtn'>
                 <img src='https://cdn-icons-png.flaticon.com/512/1972/1972090.png' alt='worksicon' width='25'/>
                 <Text fontSize='md' className='navtext'>Works</Text>
-            </a>
+            </Button>
             <Center height='25px'>
                 <Divider orientation='vertical' />
             </Center>
-            <a href='#'>
+            <Button colorScheme='teal' variant='ghost' className='navbtn'>
                 <img src='https://cdn-icons-png.flaticon.com/512/749/749064.png' alt='coursesicon' width='25'/>
                 <Text fontSize='md' className='navtext'>Courses</Text>
-            </a>
+            </Button>
             <Center height='25px'>
                 <Divider orientation='vertical' />
             </Center>
-            <a href='#'>
-                <img src='https://cdn-icons-png.flaticon.com/512/3771/3771152.png' alt='newsicon' width='25'/>
-                <Text fontSize='md' className='navtext'>News</Text>
-            </a>
+            <Button colorScheme='teal' variant='ghost' ref={btnRef} onClick={onOpen} className='navbtn'>
+                <img src='https://cdn-icons-png.flaticon.com/512/2039/2039135.png' alt='newsicon' width='25'/>
+                <Text fontSize='md' className='navtext'>More</Text>
+            </Button>
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>More info: </DrawerHeader>
+
+                <DrawerBody>
+                    <h1>WILL BE THINGS HERE</h1>
+                </DrawerBody>
+
+                <DrawerFooter>
+                    <LogoComponent/>
+                </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
         </div>
     );
 }
