e48c6a181431c1c0738277499d7ebab05f890e6c
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-03 13:11:30 +0100

    added stages without finish

--- src/components/mainpage/mainpagebody/stagesComponent.jsx
+++ src/components/mainpage/mainpagebody/stagesComponent.jsx
@@ -1,16 +1,91 @@
-import { Box, Card, CardBody, Heading, Text } from '@chakra-ui/react';
+import { CheckCircleIcon } from '@chakra-ui/icons';
+import { Box, Card, CardBody, Heading, Text, SimpleGrid, Center, CardHeader, Divider, Badge, List, ListItem, ListIcon } from '@chakra-ui/react';
 import React from 'react';
 
 const StagesComponent = () => {
     return (
         <Box w='100%' pt='1em'>
             <Heading>OpenBootcamp's stages to be Front-End Developer</Heading>
-            <Card m='1em'>
-                <CardBody>
-                    <Text fontSize='md' mt='0.5em'>Open Bootcamp was born to reinvent specialized training. Our mission is that you can have access to the highest quality technological training at no cost. With our training, you will be able to access jobs that suit your needs. Depending on the moment you are in, you can choose between our two phases:</Text>
-                </CardBody>
-            </Card>
-            <Heading>HERE WILL BE THE STEPS</Heading>
+            <SimpleGrid minChildWidth='350px' spacing='0.5em' m='1em' id='welcomegrid'>
+            <Box height='13em'>
+                <Center h='100%'>
+                    <Card height='12em'>
+                        <CardBody h='100%'>
+                            <Text fontSize='md' mt='0.5em'>Open Bootcamp was born to reinvent specialized training. Our mission is that you can have access to the highest quality technological training at no cost. With our training, you will be able to access jobs that suit your needs. Depending on the moment you are in, you can choose between our two phases:</Text>
+                        </CardBody>
+                    </Card>
+                </Center>
+            </Box>
+            <Box height='13em'>
+                <Center h='100%'>
+                    <Card w='100%' height='12em'>
+                        <CardBody h='100%' mt='0em'>
+                            <Text fontSize='xl' mt='0.1em' mb='0.5em'>
+                                <Badge variant='subtle' colorScheme='green' fontSize='1.5em'>#1</Badge>
+                            </Text>
+                            <Divider/>
+                            <List spacing={3}>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />1
+                                </ListItem>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />2
+                                </ListItem>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />3
+                                </ListItem>
+                            </List>
+                        </CardBody>
+                    </Card>
+                </Center>
+            </Box>
+            <Box height='13em'>
+                <Center h='100%'>
+                    <Card w='100%' height='12em'>
+                        <CardBody h='100%' mt='0em'>
+                            <Text fontSize='xl' mt='0.1em' mb='0.5em'>
+                                <Badge variant='subtle' colorScheme='green' fontSize='1.5em'>#1</Badge>
+                            </Text>
+                            <Divider/>
+                            <List spacing={3}>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />1
+                                </ListItem>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />2
+                                </ListItem>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />3
+                                </ListItem>
+                            </List>
+                        </CardBody>
+                    </Card>
+                </Center>
+            </Box>
+            <Box height='13em'>
+                <Center h='100%'>
+                    <Card w='100%' height='12em'>
+                        <CardBody h='100%' mt='0em'>
+                            <Text fontSize='xl' mt='0.1em' mb='0.5em'>
+                                <Badge variant='subtle' colorScheme='green' fontSize='1.5em'>#1</Badge>
+                            </Text>
+                            <Divider/>
+                            <List spacing={3}>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />1
+                                </ListItem>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />2
+                                </ListItem>
+                                <ListItem>
+                                    <ListIcon as={CheckCircleIcon} color='green.500' />3
+                                </ListItem>
+                            </List>
+                        </CardBody>
+                    </Card>
+                </Center>
+            </Box>
+        </SimpleGrid>
         </Box>
 
     );
