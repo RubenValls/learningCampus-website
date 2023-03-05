fcedbdae2887abc8a280b7ed1bf26fe12edc9945
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-05 13:02:14 +0100

    improved login page, but not working yet

--- src/components/mainpage/loginpage/loginComponent.jsx
+++ src/components/mainpage/loginpage/loginComponent.jsx
@@ -1,15 +1,19 @@
-import { Box, Button, Heading } from '@chakra-ui/react';
+import { Box, Button, Center, Heading, Image, Input } from '@chakra-ui/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginComponent = () => {
     return (
-        <Box>
-            <Heading>THIS IS LOGIN PAGE</Heading>
-            <Link to='/learningCampus-website/campus'>
-                <Button>Click Here to campus</Button>
-            </Link>
-        </Box>
+        <Center h='100vh'>
+            <Box borderWidth='1.75px' borderRadius='lg' p='2em' pt='0' borderColor='teal.400' w='30em'>
+                <Image src='https://vlctesting.es/wp-content/uploads/2022/08/Open-Bootcamp.png' w='20em' m='auto'></Image>
+                <Heading as='h2'>Write your Username</Heading>
+                <Input variant='flushed' placeholder='Username' pt='1em'/>
+                <Link to='/learningCampus-website/campus'>
+                    <Button mt='1em' colorScheme='teal' variant='solid'>Log In</Button>
+                </Link>
+            </Box>
+        </Center>
     );
 }
 
