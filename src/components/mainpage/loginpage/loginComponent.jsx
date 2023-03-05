885a159fc5ec8e148c2a29c6e09117c8a1ee2e06
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-05 13:18:35 +0100

    improved login page, but not working yet

--- src/components/mainpage/loginpage/loginComponent.jsx
+++ src/components/mainpage/loginpage/loginComponent.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const LoginComponent = () => {
     return (
         <Center h='100vh'>
-            <Box borderWidth='1.75px' borderRadius='lg' p='2em' pt='0' borderColor='teal.400' w='30em'>
+            <Box borderWidth='1.75px' borderRadius='lg' p='2em' pt='0' borderColor='teal.400' w='30em' id='loginbox'>
                 <Image src='https://vlctesting.es/wp-content/uploads/2022/08/Open-Bootcamp.png' w='20em' m='auto'></Image>
                 <Heading as='h2'>Write your Username</Heading>
                 <Input variant='flushed' placeholder='Username' pt='1em'/>
