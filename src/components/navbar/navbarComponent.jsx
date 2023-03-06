952845c39be619bb2a81bdf8799a87a5c8ec80be
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-06 12:58:47 +0100

    adding routes to navbar and working as espected

--- src/components/navbar/navbarComponent.jsx
+++ src/components/navbar/navbarComponent.jsx
@@ -12,6 +12,7 @@ import {
     DrawerCloseButton,
 } from '@chakra-ui/react'
 import LogoComponent from '../header/logoComponent';
+import { Link } from 'react-router-dom';
 
 const NavbarComponent = () => {
 
@@ -19,25 +20,31 @@ const NavbarComponent = () => {
     const btnRef = React.useRef()
 
     return (
-        <Box className='navbarcontainer'> 
-                <Button colorScheme='gray' variant='ghost' className='navbtn'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/1946/1946488.png' alt='homeicon' width='25'/>
-                    <Text fontSize='md' className='navtext'>Home</Text>
-                </Button>
+        <Box className='navbarcontainer'>
+                <Link to='/learningCampus-website/campus/welcome'>
+                    <Button colorScheme='gray' variant='ghost' className='navbtn'>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1946/1946488.png' alt='homeicon' width='25'/>
+                        <Text fontSize='md' className='navtext'>Home</Text>
+                    </Button>
+                </Link>
+                <Center height='25px'>
+                        <Divider orientation='vertical' />
+                </Center>
+                <Link to='/learningCampus-website/campus/works'>
+                    <Button colorScheme='gray' variant='ghost' className='navbtn'>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1063/1063376.png' alt='worksicon' width='25'/>
+                        <Text fontSize='md' className='navtext'>Works</Text>
+                    </Button>
+                </Link>
                 <Center height='25px'>
                     <Divider orientation='vertical' />
                 </Center>
-                <Button colorScheme='gray' variant='ghost' className='navbtn'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/1063/1063376.png' alt='worksicon' width='25'/>
-                    <Text fontSize='md' className='navtext'>Works</Text>
-                </Button>
-                <Center height='25px'>
-                    <Divider orientation='vertical' />
-                </Center>
-                <Button colorScheme='gray' variant='ghost' className='navbtn'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/2000/2000887.png' alt='coursesicon' width='25'/>
-                    <Text fontSize='md' className='navtext'>Courses</Text>
-                </Button>
+                <Link to='/learningCampus-website/campus/courses'>
+                    <Button colorScheme='gray' variant='ghost' className='navbtn'>
+                        <img src='https://cdn-icons-png.flaticon.com/512/2000/2000887.png' alt='coursesicon' width='25'/>
+                        <Text fontSize='md' className='navtext'>Courses</Text>
+                    </Button>
+                </Link>
                 <Center height='25px'>
                     <Divider orientation='vertical' />
                 </Center>
