4f47a7841c5f07de687f55b898e5c1838d00e353
Rubén Valls <r.vallsaparici@gmail.com>
2023-03-07 21:13:50 +0100

    added backend courses

--- src/components/courses/coursescardsback.jsx
+++ src/components/courses/coursescardsback.jsx
@@ -3,17 +3,56 @@ import React from 'react';
 
 const Coursescardsback = () => {
     return (
-        <SimpleGrid minChildWidth='120px' spacing='40px'>
+        <SimpleGrid minChildWidth='200px' spacing='10px' w='100%'>
             <Box height='auto'>
-                <Card maxW='sm'>
+                <Card maxW='xl' h='450px'>
                     <CardBody>
                         <Image
-                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                        alt='Green double couch with wooden legs'
+                        src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/adonisjs/adonisjs-original.svg'
+                        alt='Adonis-icon course'
                         borderRadius='lg'
                         />
-                        <Heading>BACK</Heading>
-                        <Text>DESCRIPTION</Text>
+                        <Heading>AdonisJS at Back End</Heading>
+                        <Text>Learn AdonisJs by building a production-ready application completely from scratch.</Text>
+                    </CardBody>
+                </Card>
+            </Box>
+            <Box height='auto'>
+                <Card maxW='xl' h='450px'>
+                    <CardBody>
+                        <Image
+                        src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/denojs/denojs-original.svg'
+                        alt='Deno-icon course'
+                        borderRadius='lg'
+                        />
+                        <Heading>Deno Js w/ Rust</Heading>
+                        <Text>The easiest, most secure JavaScript runtime.</Text>
+                    </CardBody>
+                </Card>
+            </Box>
+            <Box height='auto'>
+                <Card maxW='xl' h='450px'>
+                    <CardBody>
+                        <Image
+                        src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg'
+                        alt='Django-icon course'
+                        borderRadius='lg'
+                        />
+                        <Heading>Django - Practical Guide</Heading>
+                        <Text>How to build web applications with Python and Django.</Text>
+                    </CardBody>
+                </Card>
+            </Box>
+            <Box height='auto'>
+                <Card maxW='xl' h='450px'>
+                    <CardBody>
+                        <Image
+                        src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg'
+                        alt='Mongodb-icon course'
+                        borderRadius='lg'
+                        />
+                        <Heading>Developer's Guide</Heading>
+                        <Text>Master MongoDB Development for Web & Mobile Apps and CRUD Operations.</Text>
                     </CardBody>
                 </Card>
             </Box>
